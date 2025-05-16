c3deaf123d21df06ce0825f18e882a4f3ee16bdc
Andres Lampon <andrescantoran@creai.mx>
2025-05-16 02:24:00 -0600

    ENDSPOINTS POSTMAN

---
+++ Andres/Curso de FastAPI/src/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Body, Path, Query
+from fastapi.responses import HTMLResponse, JSONResponse, PlainTextResponse, RedirectResponse, FileResponse
+from pydantic import BaseModel, Field
+from typing import Optional, List
+from src.routers.movie_router import movie_router
+import datetime
+
+app = FastAPI()
+
+
+@app.get("/", tags=["Movies"])
+
+def home():
+    return PlainTextResponse(content ="Home", status_code=200)
+
+app.include_router(prefix= "/movies", router=movie_router)
+
---
+++ Andres/Curso de FastAPI/src/models/movie_model.py
@@ -0,0 +1,34 @@
+import datetime
+from operator import ge, le
+from pydantic import BaseModel, Field
+
+
+class MovieCreate(BaseModel):
+    title: str
+    year: int
+
+
+class Movie(BaseModel):
+    id: int
+    title: str
+    overview: str
+    yearr: int
+    rating: float
+    category: str
+
+class MovieCreate(BaseModel):
+    id : int
+    title : str = Field(min_length=5, max_length=15)
+    overview : str = Field(min_length=5, max_length= 50)
+    year : int = Field(le = datetime.datetime.now().year, ge= 1900)
+    rating : float = Field(ge=1 , le=10)
+    category : str = Field(min_length=5, max_length=15)
+
+
+class MovieUpdate(BaseModel):
+    tittle : str
+    overview : str
+    year : int
+    rating : float
+    category : str
+
